972382560773cf2ca1eeb7ec84ec1bc1859ab7c0
Skanouch <morgan.leitao@gmail.com>
2023-11-19 18:50:53 +0100

    up

--- start.py
+++ start.py
@@ -6,13 +6,12 @@ from get_dataframes import GetDataframes
 from tools import import_config
 
 from datetime import datetime
-time = {}
 
 start = datetime.now()
 def get_all():
     config = import_config()
-    # asyncio.run(tmdb())
-    # asyncio.run(machine_learning())
+    asyncio.run(tmdb())
+    asyncio.run(machine_learning())
     all_for_one = GetDataframes(config)
     all_for_one.get_all_dataframes()
 get_all()
